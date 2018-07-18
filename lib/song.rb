96e6e2bd037e0ded8a70296d3420c7b6d32812a3
Daniel Etter <etter.dt@gmail.com>
2018-07-18 18:51:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,6 +43,13 @@ class Song
   end
 
   def self.artist_count
-  
+    artist_hash = {}
+    @@artists.each do |artist|
+      if artist_hash[artist]
+        artist_hash[artist] +=1
+      else
+        artist_hash[artist] = 1
+      end
+    end
   end
 end
